ba5b49f57578897502405c6eb393897e6da91593
Ville Verkkonen <ville.verkkonen@solidabis.com>
2019-05-08 15:53:38 +0300

    Heroku settings

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const bodyParser = require('body-parser')
+const placesRouter = require('./controllers/places')
+const mapsRouter = require('./controllers/maps')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const options = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    keepAlive: 1000,
+    connectTimeoutMS: 30000,
+    reconnectTries: 30,
+    reconnectInterval: 2000
+}
+mongoose.connect(process.env.MONGODB_URI, options)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+
+app.use(middleware.logger)
+app.use('/api/places', placesRouter)
+app.use('/api/maps', mapsRouter)
+app.use(middleware.error)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
