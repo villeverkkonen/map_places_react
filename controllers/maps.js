b0a3cd3f7f2a2ad010346a430b4e9c68ce91d53d
Ville Verkkonen <ville.verkkonen@solidabis.com>
2019-05-21 15:12:15 +0300

    Removed test console.log

--- controllers/maps.js
+++ controllers/maps.js
@@ -1,7 +1,6 @@
 const mapsRouter = require('express').Router()
 
 mapsRouter.get('/api_key', (req, res) => {
-    console.log("apiKey: " + process.env.API_KEY)
     res.send(process.env.API_KEY)
 })
 
